c3d025f482efe9ffd80b50f14628a18a64a220ec
shentianyu <1206624123@qq.com>
2021-06-02 13:45:46 +0800

    在FirstGit类中新加一个test方法，并在main中使用这个方法

--- git-test-project/src/com/huawei/FirstGit.java
+++ git-test-project/src/com/huawei/FirstGit.java
@@ -3,5 +3,10 @@ package com.huawei;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello git project");
+        test();
+    }
+
+    public static void test(){
+        System.out.println("test");
     }
 }
